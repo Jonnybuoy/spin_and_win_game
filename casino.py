309fe01d64bef40901dc10e2187358007c7685d0
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-05-15 22:12:44 +0300

    Deposit cash, make a spin and place a bet.

--- casino.py
+++ casino.py
@@ -14,6 +14,29 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+    
+    return winnings, winning_lines
+        
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
@@ -89,8 +112,7 @@ def get_bet():
     
     return amount
 
-def main():
-    balance = deposit()
+def play(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -103,7 +125,23 @@ def main():
     
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}!")
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
 
+    
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        spin = input("Press enter to play (q to quit)")
+        if spin == "q":
+            break
+        balance += play(balance)
+    
+    print(f"You left with ${balance}")
 
 
 
